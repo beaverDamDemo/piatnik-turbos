d968444816fc95c2e160cb1f3f1df558cc4047ee
zdenevuk <zdenko.vuk@.ijs.si>
2022-07-02 15:44:26 +0200

    removed limitation to only 8 cards

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -562,8 +562,8 @@ function fillUpcars() {
     array.push(A1,B1,C1,D1,A2,B2,C2,D2,A3,B3,C3,D3,A4,B4,C4,D4,A5,B5,C5,D5,A6,B6,C6,D6,A7,B7,C7,D7,A8,B8,C8,D8);
     array.push(E1, E2, E3, E4);
 
-    // shuffle(array)
-    array.length = 8
+    shuffle(array)
+    // array.length = 8
      console.log("reduced num")
 
     return array;
