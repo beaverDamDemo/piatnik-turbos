7fd71373abe9f8157c02a50288dbd04a174bcc56
zdenevuk <zdenko.vuk@.ijs.si>
2022-07-03 18:12:17 +0200

    moved cars to an outside file. Added piatnik tuning cards which's data is not completly inserted yet.

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -561,43 +561,43 @@ function fillUpcars() {
             return "won: "+this.duelsWon+", tie: "+this.duelsTie+", lost: "+this.duelsLost
         }
     }
-    var A1 = new Car('1A', 'Toyota Celica 4 WD', 4, 150, 1998, 230, 'assets/images/cars/toyota_celica.jpeg', 2, 3);
-    var B1 = new Car('1B', 'Porsche 939 Turbo', 4, 221, 3300, 260, 'assets/images/cars/p939.jpg', 2, 1);
-    var C1 = new Car('1C', 'Opel Manta', 4, 110, 1956, 216.8, 'assets/images/cars/opel_manta.jpg', 3, 0);
-    var D1 = new Car('1D', 'Bmw 745i', 6, 185, 3430, 228, 'assets/images/cars/bmw745.jpg', 2, 2);
-    var A2 = new Car('2A', 'Porsche 924 Carrera GT', 4, 154.5, 1984, 237, 'assets/images/cars/p924.jpg', 3, 0);
-    var B2 = new Car('2B', 'Saab 900', 4, 107, 1985, 195, 'assets/images/cars/saab900.jpg', 0, 2);
-    var C2 = new Car('2C', 'Mitsubishi Cordia', 4, 84, 1597, 184, 'assets/images/cars/mitsubishi_cordia.jpg', 0, 2);
-    var D2 = new Car('2D', 'Renault 5', 4, 118, 1397, 210, 'assets/images/cars/renault5.jpg', 0, 3);
-    var A3 = new Car('3A', 'MG Metro', 4, 69, 1275, 180, 'assets/images/cars/mg_metro.jpg', 0, 0);
-    var B3 = new Car('3B', 'Ford Capri', 6, 138, 2793, 210, 'assets/images/cars/ford.jpg', 2, 0);
-    var C3 = new Car('3C', 'Mitsubishi Starion', 4, 125, 1996, 220, 'assets/images/cars/mitsubishi_starion.jpg', 0, 3);
-    var D3 = new Car('3D', 'Audi Quattro', 5, 147, 2144, 220, 'assets/images/cars/audi_quattro.jpg', 0, 2);
-    var A4 = new Car('4A', 'Mazda RX7', 2, 122, 2292, 225, 'assets/images/cars/mazda_rx7.jpg',  2, 3);
-    var B4 = new Car('4A', 'Porsche C.', 6, 213, 2687, 165, 'assets/images/cars/911c.jpg', 0, 1);
-    var C4 = new Car('4C', 'Citroen CX GTI', 4, 124, 2500, 220, 'assets/images/cars/citroen_cx.jpg', 2, 0);
-    var D4 = new Car('4D', 'Audi 100', 5, 121, 2228, 213, 'assets/images/cars/audi_100.jpg', 0, 2);
-    var A5 = new Car('5A', 'Lotus Esprit S3', 4, 156, 2172, 244.6, 'assets/images/cars/lotus_esprit.jpg', 3, 0);
-    var B5 = new Car('5B', 'TVR 100', 6, 169, 3000, 230, 'assets/images/cars/tvr.jpg', 2, 0);
-    var C5 = new Car('5C', 'Fiat UNO 75 i.e. Turbo Kat', 4, 74, 1301, 190, 'assets/images/cars/fiat_uno.jpg', 0, 3);
-    var D5 = new Car('5D', 'Lancia Delta HF Integrale', 4, 183, 1981, 215, 'assets/images/cars/lancia_delta.jpg', 1, 0);
-    var A6 = new Car('6A', 'Lamborghini Countach LP500S', 12, 276, 4754, 293, 'assets/images/cars/countach.jpg', 0, 2);
-    var B6 = new Car('6B', 'Ferrari 308 GTBi-K&#246nig', 8, 232, 2927, 264.7, 'assets/images/cars/308.jpg', 0, 1);
-    var C6 = new Car('6C', 'BMW 2002', 4, 265, 1426, 255, 'assets/images/cars/bmw2002.jpg', 1, 3);
-    var D6 = new Car('6D', 'Porsche 911 Turbo', 6, 235, 3300, 270, 'assets/images/cars/p911.jpg', 2, 3);
-    var A7 = new Car('7A', 'Nissan 300 ZX', 6, 208, 2959, 250, 'assets/images/cars/nissan300.jpg', 2, 1);
-    var B7 = new Car('7B', 'Alfetta GTV', 4, 294, 1421, 290, 'assets/images/cars/alfetta-gtv.jpg', 1, 3);
-    var C7 = new Car('7C', 'Porsche 934', 6, 441, 2993, 305, 'assets/images/cars/porsche-934.jpg', 1, 3);
-    var D7 = new Car('7D', 'Ford Escort', 4, 279, 1427, 270, 'assets/images/cars/ford_escort.jpg', 1, 3);
-    var A8 = new Car('8A', 'Ferrari F40', 8, 351, 2936, 324, 'assets/images/cars/f40.jpg', 3, 3);
-    var B8 = new Car('8B', 'Ferrari GTB', 8, 198, 3185, 263, 'assets/images/cars/f328.jpg', 0, 2);
-    var C8 = new Car('8C', 'Toyota Supra Katarga', 6, 173, 2954, 245, 'assets/images/cars/toyota_supra.jpg', 2, 0);
-    var D8 = new Car('8D', 'Porsche 944', 4, 184, 2479, 260, 'assets/images/cars/porsche944.jpg', 3, 2);
+    var A1 = new Car(_A1[0], _A1[1], _A1[2], _A1[3], _A1[4], _A1[5], _A1[6], _A1[7])
+    var B1 = new Car(_B1[0], _B1[1], _B1[2], _B1[3], _B1[4], _B1[5], _B1[6], _B1[7])
+    var C1 = new Car(_C1[0], _C1[1], _C1[2], _C1[3], _C1[4], _C1[5], _C1[6], _C1[7])
+    var D1 = new Car(_D1[0], _D1[1], _D1[2], _D1[3], _D1[4], _D1[5], _D1[6], _D1[7])
+    var A2 = new Car(_A2[0], _A2[1], _A2[2], _A2[3], _A2[4], _A2[5], _A2[6], _A2[7])
+    var B2 = new Car(_B2[0], _B2[1], _B2[2], _B2[3], _B2[4], _B2[5], _B2[6], _B2[7])
+    var C2 = new Car(_C2[0], _C2[1], _C2[2], _C2[3], _C2[4], _C2[5], _C2[6], _C2[7])
+    var D2 = new Car(_D2[0], _D2[1], _D2[2], _D2[3], _D2[4], _D2[5], _D2[6], _D2[7])
+    var A3 = new Car(_A3[0], _A3[1], _A3[2], _A3[3], _A3[4], _A3[5], _A3[6], _A3[7])
+    var B3 = new Car(_B3[0], _B3[1], _B3[2], _B3[3], _B3[4], _B3[5], _B3[6], _B3[7])
+    var C3 = new Car(_C3[0], _C3[1], _C3[2], _C3[3], _C3[4], _C3[5], _C3[6], _C3[7])
+    var D3 = new Car(_D3[0], _D3[1], _D3[2], _D3[3], _D3[4], _D3[5], _D3[6], _D3[7])
+    var A4 = new Car(_A4[0], _A4[1], _A4[2], _A4[3], _A4[4], _A4[5], _A4[6], _A4[7])
+    var B4 = new Car(_B4[0], _B4[1], _B4[2], _B4[3], _B4[4], _B4[5], _B4[6], _B4[7])
+    var C4 = new Car(_C4[0], _C4[1], _C4[2], _C4[3], _C4[4], _C4[5], _C4[6], _C4[7])
+    var D4 = new Car(_D4[0], _D4[1], _D4[2], _D4[3], _D4[4], _D4[5], _D4[6], _D4[7])
+    var A5 = new Car(_A5[0], _A5[1], _A5[2], _A5[3], _A5[4], _A5[5], _A5[6], _A5[7])
+    var B5 = new Car(_B5[0], _B5[1], _B5[2], _B5[3], _B5[4], _B5[5], _B5[6], _B5[7])
+    var C5 = new Car(_C5[0], _C5[1], _C5[2], _C5[3], _C5[4], _C5[5], _C5[6], _C5[7])
+    var D5 = new Car(_D5[0], _D5[1], _D5[2], _D5[3], _D5[4], _D5[5], _D5[6], _D5[7])
+    var A6 = new Car(_A6[0], _A6[1], _A6[2], _A6[3], _A6[4], _A6[5], _A6[6], _A6[7])
+    var B6 = new Car(_B6[0], _B6[1], _B6[2], _B6[3], _B6[4], _B6[5], _B6[6], _B6[7])
+    var C6 = new Car(_C6[0], _C6[1], _C6[2], _C6[3], _C6[4], _C6[5], _C6[6], _C6[7])
+    var D6 = new Car(_D6[0], _D6[1], _D6[2], _D6[3], _D6[4], _D6[5], _D6[6], _D6[7])
+    var A7 = new Car(_A7[0], _A7[1], _A7[2], _A7[3], _A7[4], _A7[5], _A7[6], _A7[7])
+    var B7 = new Car(_B7[0], _B7[1], _B7[2], _B7[3], _B7[4], _B7[5], _B7[6], _B7[7])
+    var C7 = new Car(_C7[0], _C7[1], _C7[2], _C7[3], _C7[4], _C7[5], _C7[6], _C7[7])
+    var D7 = new Car(_D7[0], _D7[1], _D7[2], _D7[3], _D7[4], _D7[5], _D7[6], _D7[7])
+    var A8 = new Car(_A8[0], _A8[1], _A8[2], _A8[3], _A8[4], _A8[5], _A8[6], _A8[7])
+    var B8 = new Car(_B8[0], _B8[1], _B8[2], _B8[3], _B8[4], _B8[5], _B8[6], _B8[7])
+    var C8 = new Car(_C8[0], _C8[1], _C8[2], _C8[3], _C8[4], _C8[5], _C8[6], _C8[7])
+    var D8 = new Car(_D8[0], _D8[1], _D8[2], _D8[3], _D8[4], _D8[5], _D8[6], _D8[7])
 
-    var E1 = new Car('1E', 'Dauer 962', 6, 552, 2994, 406, 'assets/images/cars/dauer-962.jpg', 3, 1);
-    var E2 = new Car('2E', 'Volvo 850 T-5R' , 5, 181, 2319, 245, 'assets/images/cars/volvo-850.jpg', 0, 1);
-    var E3 = new Car('3E', 'Alfa 164 Turbo 8v', 4, 129, 1995, 225, 'assets/images/cars/alfa-164.jpg', 3, 1);
-    var E4 = new Car('4E', 'Ford Escort Cosworth', 4, 167, 1994, 232, 'assets/images/cars/ford-escort-rs.jpg', 2, 0);
+    var E1 = new Car(_E1[0], _E1[1], _E1[2], _E1[3], _E1[4], _E1[5], _E1[6], _E1[7])
+    var E2 = new Car(_E2[0], _E2[1], _E2[2], _E2[3], _E2[4], _E2[5], _E2[6], _E2[7])
+    var E3 = new Car(_E3[0], _E3[1], _E3[2], _E3[3], _E3[4], _E3[5], _E3[6], _E3[7])
+    var E4 = new Car(_E4[0], _E4[1], _E4[2], _E4[3], _E4[4], _E4[5], _E4[6], _E4[7])
 
     array.push(A1,B1,C1,D1,A2,B2,C2,D2,A3,B3,C3,D3,A4,B4,C4,D4,A5,B5,C5,D5,A6,B6,C6,D6,A7,B7,C7,D7,A8,B8,C8,D8);
     array.push(E1, E2, E3, E4);
