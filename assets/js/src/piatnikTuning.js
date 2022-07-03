7b1f9237a8781b0a4f76dc23ed2988f8cb57bfae
zdenevuk <zdenko.vuk@.ijs.si>
2022-07-03 18:25:58 +0200

    added some furhter typing from cards to piatnik tuning

--- assets/js/src/piatnikTuning.js
+++ assets/js/src/piatnikTuning.js
@@ -1,34 +1,67 @@
 
 // var A1 = ['1A', 'Toyota Celica 4 WD', 4, 150, 1998, 230, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 3]
-var A1 = ['1A', 'Nissan Lady-Micra', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var B1 = ['1B', 'Citroen Visa GTI Creation df', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var C1 = ['1C', 'Lotus M 200', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var D1 = ['1D', 'Wood & Pickett Laser Metro', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var A2 = ['2A', 'Arex Roadster', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var B2 = ['2B', 'Pfeba Renault 21', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var C2 = ['2C', 'Irmscher Kadett E', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var D2 = ['2D', 'Nissan Silvia SR-X', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var A3 = ['3A', 'AMG 190 E 3,2', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var B3 = ['3B', 'Ledi', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var C3 = ['3C', 'Treser Golf GTI', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var D3 = ['3D', 'Ford RS 200', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var A4 = ['4A', 'Daimler Benz 190 E', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var B4 = ['4A', 'Porsche 924 break', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var C4 = ['4C', 'AMG S-Klasse', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var D4 = ['4D', 'Nissan CUE-X', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var A5 = ['5A', 'Sbarro Shahin 1000', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var B5 = ['5B', 'Mercedes ABC-Exclusive', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var C5 = ['5C', 'BMW 635 ABC', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var D5 = ['5D', 'Audi quattro Sport', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var A6 = ['6A', 'AMG Typ W 126-5,4', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var B6 = ['6B', 'Nissan MID-4 (Sport coupe)', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var C6 = ['6C', 'Porsche 939 Turbo', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var D6 = ['6D', 'Ferrari 308 GTBi-Konig', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var A7 = ['7A', 'AMG Typ W 125C-5,0', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var B7 = ['7B', 'Porsche 911 2,7 Meyer', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var C7 = ['7C', 'Porsche Gemballa Avalanche', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var D7 = ['7D', 'Porsche 911 Turbo TAG', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var A8 = ['8A', 'AMG 500 SL 6,0', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var B8 = ['8B', 'ISDERA Imperator 108i', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var C8 = ['8C', 'Wolf Lamborghini', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
-var D8 = ['8D', 'Metalex Tatra', 111, 222, 333, 444, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 555, 666]
+var A1 = ['1A', , 111, 222, 998, 140, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var B1 = ['1B', , 111, 222, 1569, 188, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var C1 = ['1C', , 111, 222, 1600, 196, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var D1 = ['1D', , 111, 222, 1256, 190, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var A2 = ['2A', , 111, 222, 5700, 345, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var B2 = ['2B', , 111, 222, 1995, 192, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var C2 = ['2C', , 111, 222, 1771, 210, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var D2 = ['2D', , 111, 222, 1991, 205, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var A3 = ['3A', , 111, 222, 3200, 244, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var B3 = ['3B', , 111, 222, 1597, 240, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var C3 = ['3C', , 111, 222, 1891, 225, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var D3 = ['3D', , 111, 222, 1804, 240, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var A4 = ['4A', , 111, 222, 1997, 215, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var B4 = ['4A', , 111, 222, 1984, 230, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var C4 = ['4C', , 111, 222, 6000, 250, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var D4 = ['4D', , 111, 222, 3000, 240, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var A5 = ['5A', , 111, 222, 4973, 235, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var B5 = ['5B', , 111, 222, 5547, 250, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var C5 = ['5C', , 111, 222, 3453, 255, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var D5 = ['5D', , 111, 222, 2133, 250, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var A6 = ['6A', , 111, 222, 4973, 250, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var B6 = ['6B', , 111, 222, 3000, 250, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var C6 = ['6C', , 111, 222, 3300, 260, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var D6 = ['6D', , 111, 222, 2927, 265, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var A7 = ['7A', , 111, 222, 4973, 263, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var B7 = ['7B', , 111, 222, 2697, 254, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var C7 = ['7C', , 111, 222, 3400, 290, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var D7 = ['7D', , 111, 222, 3297, 292, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var A8 = ['8A', , 111, 222, 6000, 250, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var B8 = ['8B', , 111, 222, 5000, 258, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var C8 = ['8C', , 111, 222, 5000, 296, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+var D8 = ['8D', , 111, 222, 4000, 265, 'assets/images/cars/piatnik tuning 76fb3b_o.jpg', 2, 4]
+
+'Nissan Lady-Micra'
+'Citroen Visa GTI Creation df'
+'Lotus M 200'
+'Wood & Pickett Laser Metro'
+'Arex Roadster'
+'Pfeba Renault 21'
+'Irmscher Kadett E'
+'Nissan Silvia SR-X'
+'AMG 190 E 3,2'
+'Ledi'
+'Treser Golf GTI'
+'Ford RS 200'
+'Daimler Benz 190 E'
+'Porsche 924 break'
+'AMG S-Klasse'
+'Nissan CUE-X'
+'Sbarro Shahin 1000'
+'Mercedes ABC-Exclusive'
+'BMW 635 ABC'
+'Audi quattro Sport'
+'AMG Typ W 126-5,4'
+'Nissan MID-4 (Sport coupe)'
+'Porsche 939 Turbo'
+'Ferrari 308 GTBi-Konig'
+'AMG Typ W 125C-5,0'
+'Porsche 911 2,7 Meyer'
+'Porsche Gemballa Avalanche'
+'Porsche 911 Turbo TAG'
+'AMG 500 SL 6,0'
+'ISDERA Imperator 108i'
+'Wolf Lamborghini'
+'Metalex Tatra'
