f4e810e6c54b5c7837a9c8b6bca438c9e8804e44
zdenevuk <zdenko.vuk@.ijs.si>
2023-01-17 22:59:20 +0100

    wip 2

--- assets/js/src/flotteFlitzer.js
+++ assets/js/src/flotteFlitzer.js
@@ -1,667 +1,553 @@
-2850;
-3299;
-5600;
-2184;
-6;
-8;
-4;
-4942;
-12;
-221;
-331;
-108;
-5500;
-280;
-6000;
-4300;
-240.0;
-305;
-150.0;
+// 5500
+// 6000
+// 4300
+// 240.0
 
-205;
-331;
-6500;
-320;
-420.0;
+// 150.0
 
-30.0;
+// 6500
 
-287;
-6300;
-290;
-240.0;
+// 420.0
+// 30.0
 
-2936;
-8;
-351;
-7000;
-324;
-444.0;
+// 6300
 
-5733;
-5700;
-8;
-8;
-281;
-4250;
-304;
-150.0;
+// 240.0
 
-447;
-6000;
-389;
-450.0;
+// 4250
+// 150.0
+// 6000
+// 450.0
+// 3590
+// 6
+// 6000
+// 130.0
+// 145
+// 5200
+// 245
+// 74.0
+// 228
+// 60.0
 
-4973;
-3590;
-8;
-6;
-228;
-6000;
-300;
-130.0;
-
-145;
-5200;
-245;
-74.0;
-
-1948;
-4;
-92;
-5500;
-190;
-33.5;
-
-2790;
-6;
-184;
-5600;
-245;
-120.0;
-
-5341;
-8;
-318;
-6250;
-299;
-400.0;
-
-2000;
-110;
-6500;
-230;
-43.0;
-
-5995;
-16;
-412;
-8000;
-328;
-527.25;
-1380;
-76;
-6200;
-195;
-39.5;
-
-3185;
-5167;
-8;
-12;
-2664;
-199;
-7000;
-260;
-250.0;
-
-335;
-7000;
-298;
-235.0;
-
-6;
-105;
-5500;
-190;
-40.0;
-
-2959;
-154;
-6200;
-245;
-100.0;
-
-1993;
-150;
-6000;
-240;
-49.5;
-
-4087;
-8;
-127;
-4300;
-194;
-145.0;
-
-5547;
-8;
-220;
-5600;
-282;
-375.0;
-
-5769;
-8;
-221;
-6000;
-250;
-145.0;
-
-5547;
-8;
-368;
-6500;
-300;
-250.0;
-
-2174;
-160;
-6000;
-245;
-92.9;
-2855;
-8;
-294;
-7000;
-303;
-500.0;
-
-3485;
-8;
-188;
-7000;
-243;
-118.5;
-
-5733;
-8;
-168;
-4400;
-4228;
-5002;
-2458;
-8;
-8;
-241;
-85.0;
-
-195;
-5500;
-172;
-3200;
-215;
-48.5;
-
-6;
-147;
-6100;
-250;
-108.0;
-228;
-60.0;
 
 var _A1 = [
   "1A",
   "Sbarro Challenge III",
-  16,
-  333,
-  5555,
-  222,
+  6,
+  221,
+  3299,
+  280,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   0 * 320,
   0,
-];
+]
 
 var _A2 = [
   "2A",
-  "Jehle Pantera",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  331,
+  5600,
+  305,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   1 * 320,
   0,
-];
+]
 
 var _A3 = [
   "3A",
   "Ford Probe GT",
-  16,
-  333,
-  5555,
-  222,
+  4,
+  108,
+  2184,
+  205,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   2 * 320,
   0,
-];
+]
 
 var _A4 = [
   "4A",
   "Porsche 959",
-  16,
-  333,
-  5555,
-  222,
+  6,
+  331,
+  2850,
+  320,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   3 * 320,
   0,
-];
+]
 
 var _A5 = [
   "5A",
   "Ferrari Testarossa",
-  16,
-  333,
-  5555,
-  222,
+  12,
+  287,
+  4942,
+  290,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (5 - 1) * 320,
   0,
-];
+]
 
+// 6500
+// 43.0
 var _A6 = [
   "6A",
   "Jehle Saphir",
-  16,
-  333,
-  5555,
-  222,
+  4,
+  110,
+  2000,
+  230,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (6 - 1) * 320,
   0,
-];
+]
 
+// 8000
+// 527.25
 var _A7 = [
   "7A",
   "Cizeta Moroder V16T",
 
   16,
-  333,
-  5555,
-  222,
+  412,
+  5995,
+  328,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (7 - 1) * 320,
   0,
-];
+]
 
+// 6200
+// 39.5
 var _A8 = [
   "8A",
   "Midas Gold",
-  16,
-  333,
-  5555,
-  222,
+  4,
+  76,
+  1380,
+  195,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (8 - 1) * 320,
   0,
-];
+]
 
 var _B1 = [
   "1B",
   "Ferrari F 40",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  351,
+  2936,
+  324,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   0 * 320,
   540,
-];
+]
 
 var _B2 = [
   "2B",
   "Callaway Corvette Twin Turbo",
-
-  16,
-  333,
-  5555,
-  222,
+  8,
+  281,
+  5733,
+  304,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   1 * 320,
   540,
-];
+]
 
 var _B3 = [
   "3B",
   "Vector W2 Twin Turbo",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  447,
+  5700,
+  389,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   2 * 320,
   540,
-];
+]
 
+// 7000
+// 250.0
 var _B4 = [
   "4B",
   "Sbarro Ferrari P4",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  199,
+  3185,
+  260,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   3 * 320,
   540,
-];
+]
 
+// 7000
+// 235.0
 var _B5 = [
   "5B",
   "Lamborghini Countach",
-  16,
-  333,
-  5555,
-  222,
+  12,
+  335,
+  5167,
+  298,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (5 - 1) * 320,
   540,
-];
+]
 
+// 5500
+// 40.0
 var _B6 = [
   "6B",
   "Sethera",
-  16,
-  333,
-  5555,
-  222,
+  6,
+  105,
+  2664,
+  190,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (6 - 1) * 320,
   540,
-];
+]
 
+// 6200
+// 100.0
 var _B7 = [
   "7B",
   "Alfa Romeo SZ",
-  16,
-  333,
-  5555,
-  222,
+  6,
+  154,
+  2959,
+  245,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (7 - 1) * 320,
   540,
-];
+]
 
+// 6000
+// 49.5
 var _B8 = [
   "8B",
   "Ford Sierra RS Cosworth",
-  16,
-  333,
-  5555,
-  222,
+  4,
+  150,
+  1993,
+  240,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (8 - 1) * 320,
   540,
-];
+]
 
 var _C1 = [
   "1C",
   "Arrow C1",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  228,
+  4973,
+  300,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   0 * 320,
   1080,
-];
+]
 
+// 5200  km/h
+// 74.000,- DM
 var _C2 = [
   "2C",
   "Irmscher GT",
-  16,
-  333,
-  5555,
-  222,
+  6,
+  145,
+  3590,
+  245,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   1 * 320,
   1080,
-];
+]
 
+// Aston Martin Zagato
+// 5341 ccm
+// 8
+// 318/432
+// 6250
+// 299 km/h
+// 400.000,- DM
+// 6250
+// 400.0
 var _C3 = [
   "3C",
   "Aston Martin Zagato",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  318,
+  5341,
+  299,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   2 * 320,
   1080,
-];
+]
 
+
+// Cadillac Allante
+//  ccm
+
+// /172
+// 4300  km/h
+// 145.000,- DM
 var _C4 = [
   "4C",
   "Cadillac Allante",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  127,
+  4087,
+  194,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   3 * 320,
   1080,
-];
+]
 
+
+// Isdera Imperator 108i
+//  ccm
+
+// /300 5600
+//  km/h
+// 375.000,- DM
 var _C5 = [
   "5C",
   "Isdera Imperator 108i",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  220,
+  5547,
+  282,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (5 - 1) * 320,
   1080,
-];
+]
 
+
+// De Tomaso Pantera
+//  ccm
+// /300 6000
+//  km/h 145.000,- DM
 var _C6 = [
   "6C",
   "De Tomaso Pantera",
-
-  16,
-  333,
-  5555,
-  222,
+  8,
+  221,
+  5769,
+  250,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (6 - 1) * 320,
   1080,
-];
+]
 
+// Koenig Mercedes 560 SEC
+//  ccm
+
+// /500
+// 6500
+//  km/h
+// 250.000,-DM
 var _C7 = [
   "7C",
   "Koenig Mercedes 560 SEC",
-
-  16,
-  333,
-  5555,
-  222,
+  8,
+  368,
+  5547,
+  300,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (7 - 1) * 320,
   1080,
-];
+]
+
+// Lotus Esprit Turbo
+// /218
+// 6000
+//  km/h
+// 92.900,-DM
 
 var _C8 = [
   "8C",
   "Lotus Esprit Turbo",
-  16,
-  333,
-  5555,
-  222,
+  4,
+  160,
+  2174,
+  245,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (8 - 1) * 320,
   1080,
-];
+]
 
+// 5500
+// 33.5
 var _D1 = [
   "1D",
   "Alfa Romeo Spider",
-
-  16,
-  333,
-  5555,
-  222,
+  4,
+  92,
+  1948,
+  190,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   0 * 320,
   1620,
-];
+]
 
+// 5600
+// 120.0
 var _D2 = [
   "2D",
   "Maserati Karif",
-
-  16,
-  333,
-  5555,
-  222,
+  6,
+  184,
+  2790,
+  245,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   1 * 320,
   1620,
-];
+]
 
+// 7000
+// 500.0
 var _D3 = [
   "3D",
   "Ferrari GTO",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  294,
+  2855,
+  303,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   2 * 320,
   1620,
-];
+]
 
+// 7000
+// 118.5
 var _D4 = [
   "4D",
   "Lamborghini Jalpa",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  188,
+  3485,
+  243,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   3 * 320,
   1620,
-];
+]
 
+// 4400
+// 60.0
 var _D5 = [
   "5D",
   "Pontiac Firebird GTA",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  168,
+  5733,
+  228,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (5 - 1) * 320,
   1620,
-];
+]
 
+// 85.0
+// 5500
 var _D6 = [
   "6D",
   "TVR 420 Saloon",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  195,
+  4228,
+  241,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (6 - 1) * 320,
   1620,
-];
-
+]
+// 3200
+// 48.5
 var _D7 = [
   "7D",
   "Chevrolet Camaro",
-  16,
-  333,
-  5555,
-  222,
+  8,
+  172,
+  5002,
+  215,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (7 - 1) * 320,
   1620,
-];
+]
 
+// 6100
+// 108.0
 var _D8 = [
   "8D",
   "MVS Venturi",
-  16,
-  333,
-  5555,
-  222,
+  6,
+  147,
+  2458,
+  250,
   "assets/images/flotteFlitzer/flotteFlitzer.jpg",
   2,
   3,
   (8 - 1) * 320,
   1620,
-];
+]
