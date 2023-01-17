5cd968c560b90d2a063a23d9274a2db23376d8a0
zdenevuk <zdenko.vuk@.ijs.si>
2023-01-17 16:05:09 +0100

    added flotte flitzer

---
+++ assets/js/src/flotteFlitzer.js
@@ -0,0 +1,810 @@
+
+Porsche 959
+Sbarro Challenge III
+Jehle Pantera
+Ford Probe GT
+Ferrari Testarossa
+Jehle Saphir
+Hubraum:
+2850 ccm
+Hubraum:
+3299 ccm
+Hubraum:
+5600 ccm
+Hubraum:
+2184 ccm
+Hubraum:
+Zylinder:
+Zylinder:
+6
+Zylinder:
+8
+Zylinder:
+4
+Zylinder:
+4942 ccm 12
+kW/PS:
+kW/PS:
+221/300
+kW/PS:
+331/450
+kW/PS:
+108/147
+Umdreh./Min.:
+Umdreh./Min.:
+Geschwindigkeit:
+5500
+280 km/h
+Umdreh./Min.:
+6000
+Umdreh./Min.:
+4300
+Geschwindigkeit:
+Geschwindigkeit:
+Preis:
+240.000,- DM
+Preis:
+305 km/h 150.000,- DM
+Geschwindigkeit:
+205 km/h
+Preis:
+331/450 6500
+320 km/h 420.000,- DM
+kW/PS: Umdreh./Min.: Geschwindigkeit:
+Preis:
+30.000,- DM
+Preis:
+287/390 6300 290 km/h 240.000,- DM
+Hubraum: Zylinder: kW/PS: Umdreh./Min.: Geschwindigkeit:
+Preis:
+1B
+Ferrari F 40
+2B
+Callaway Corvette Twin Turbo
+Hubraum:
+2936 ccm
+Hubraum:
+Zylinder:
+8
+Zylinder:
+kW/PS:
+351/478
+kW/PS:
+Umdreh./Min.:
+7000
+Umdreh./Min.:
+Geschwindigkeit:
+324 km/h
+Geschwindigkeit:
+Preis:
+444.000,- DM
+Preis:
+IC
+Arrow C1
+Hubraum:
+Zylinder:
+kW/PS:
+Umdreh./Min.:
+Geschwindigkeit:
+Preis:
+1D
+2C
+Irmscher GT
+3B
+Vector W2 Twin Turbo
+4B
+Sbarro Ferrari P4
+Hubraum:
+5733 ccm
+Hubraum:
+5700 ccm
+Zylinder:
+8
+Zylinder:
+8
+kW/PS:
+281/382 4250 304 km/h
+150.000,- DM
+Preis:
+kW/PS:
+447/600
+Umdreh./Min.:
+Umdreh./Min.:.
+6000
+Geschwindigkeit:
+Geschwindigkeit:
+389 km/h
+Preis:
+450.000,- DM
+3C
+Aston Martin Zagato
+4973 ccm
+Hubraum:
+3590 ccm
+Hubraum:
+8
+Zylinder:
+6
+Zylinder:
+228/310 6000 300 km/h
+130.000,- DM
+kW/PS:
+Umdreh./Min.: Geschwindigkeit:
+145/197 5200 245 km/h
+kW/PS:
+Umdreh./Min.:
+Geschwindigkeit:
+Preis:
+74.000,- DM
+Preis:
+Alfa Romeo Spider
+Hubraum:
+Zylinder:
+kW/PS:
+Umdreh./Min.:
+1948 ccm
+4
+92/125 5500
+Geschwindigkeit:
+Preis:
+190 km/h
+33.500,- DM
+2D
+Maserati Karif
+Hubraum:
+Zylinder:
+kW/PS:
+3D
+Ferrari GTO
+Hubraum:
+2790 ccm
+6
+Zylinder:
+184/250
+kW/PS:
+Umdreh./Min.:
+Umdreh./Min.:
+Geschwindigkeit:
+Preis:
+5600 245 km/h
+120.000,- DM
+4C
+Cadillac Allante
+5341 ccm
+Hubraum:
+8
+Zylinder:
+318/432
+kW/PS:
+6250
+299 km/h
+400.000,- DM
+Umdreh./Min.:
+Geschwindigkeit:
+Preis:
+4D
+5B
+6B
+Cizeta Moroder V16T
+2000 ccm
+Hubraum: Zylinder:
+110/150 6500
+kW/PS: Umdreh/Min.: Geschwindigkeit:
+230 km/h 43.000,-DM
+Preis:
+7B
+Midas Gold
+5995 com 16 412/560 8000 328 km/h 527.250,-DM
+Hubraum: Zylinder: KW/PS:
+1380.com
+76/102
+Umdreh/Min: Geschwindigkeit
+Preis:
+6200
+195 km/h 39.500,-DM
+8B
+Lamborghini Countach
+Sethera
+Alfa Romeo SZ
+3185 ccm
+Hubraum:
+5167 ccm
+8
+Zylinder:
+12
+Hubraum: Zylinder:
+2664 com
+Hubraum:
+199/270
+kW/PS:
+7000 260 km/h 250.000,- DM
+Umdreh./Min.:
+335/455 7000
+kW/PS:
+Geschwindigkeit:
+298 km/h
+Umdreh./Min.: Geschwindigkeit:
+Preis:
+235.000,- DM
+Preis:
+6 105/142 5500 190 km/h 40.000,-DM
+Zylinder:
+kW/PS: Umdreh./Min.: Geschwindigkeit:
+Preis:
+2959 ccm
+154/210
+6200
+245 km/h 100.000,- DM
+Ford Sierra RS Cosworth
+Hubraum:
+Zylinder:
+kW/PS:
+Umdreh./Min.: Geschwindigkeit:
+Preis:
+1993 ccm
+150/204
+6000
+240 km/h 49.500,-DM
+5C
+Isdera Imperator 108i
+6C
+De Tomaso Pantera
+7C
+Koenig Mercedes 560 SEC
+8C
+Lotus Esprit Turbo
+4087 ccm
+Hubraum:
+8
+Zylinder:
+127/172 4300
+kW/PS:
+194 km/h 145.000,- DM
+Umdreh./Min.: Geschwindigkeit:
+Preis:
+5547 ccm
+Hubraum:
+8
+Zylinder:
+220/300
+kW/PS:
+5600
+Umdreh./Min.:
+282 km/h 375.000,- DM
+Geschwindigkeit:
+Preis:
+5769 ccm
+Hubraum:
+8
+Zylinder:
+221/300
+6000
+250 km/h 145.000,- DM
+kW/PS:
+Umdreh./Min.: Geschwindigkeit:
+Preis:
+5547 ccm
+Hubraum:
+8
+Zylinder:
+368/500 6500
+300 km/h 250.000,-DM
+kW/PS:
+Umdreh./Min.:
+Geschwindigkeit:
+Preis:
+2174.com
+160/218
+6000 245 km/h
+92.900,-DM
+Lamborghini Jalpa
+2855 ccm
+Hubraum:
+8
+Zylinder:
+kW/PS:
+Umdreh./Min.:
+Geschwindigkeit:
+Preis:
+294/400 7000 303 km/h 500.000,- DM
+5D
+Pontiac Firebird GTA
+3485 ccm
+Hubraum:
+8
+Zylinder:
+188/255 7000
+Geschwindigkeit:
+Preis:
+243 km/h 118.500,- DM
+6D
+7D
+8D
+TVR 420 Saloon
+Chevrolet Camaro
+MVS Venturi
+5733 ccm
+Hubraum:
+8
+Zylinder:
+168/228
+4400
+4228 ccm
+Hubraum:
+5002 ccm
+Hubraum:
+2458 com
+8
+Zylinder:
+8
+Zylinder:
+kW/PS:
+Umdreh./Min.: Geschwindigkeit:
+Preis:
+241 km/h 85.000,- DM
+kW/PS:
+Umdreh./Min.: Geschwindigkeit:
+Preis:
+195/265
+5500
+172/233 3200 215 km/h
+48.500,-DM
+Preis:
+kW/PS:
+Umdreh/Min.:
+Geschwindigkeit:
+6 147/200 6100 250 km/h 108.000-DM
+kW/PS:
+Umdreh./Min.: Geschwindigkeit:
+Preis:
+228 km/h 60.000,- DM
+
+
+
+var _A1 = [
+  "1A",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  0 * 320,
+  0,
+];
+
+var _A2 = [
+  "2A",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  1 * 320,
+  0,
+];
+
+var _A3 = [
+  "3A",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  2 * 320,
+  0,
+];
+
+var _A4 = [
+  "4A",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  3 * 320,
+  0,
+];
+
+var _A5 = [
+  "5A",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (5 - 1) * 320,
+  0,
+];
+
+var _A6 = [
+  "6A",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (6 - 1) * 320,
+  0,
+];
+
+var _A7 = [
+  "7A",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (7 - 1) * 320,
+  0,
+];
+
+var _A8 = [
+  "8A",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (8 - 1) * 320,
+  0,
+];
+
+var _B1 = [
+  "1B",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  0 * 320,
+  540,
+];
+
+var _B2 = [
+  "2B",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  1 * 320,
+  540,
+];
+
+var _B3 = [
+  "3B",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  2 * 320,
+  540,
+];
+
+var _B4 = [
+  "4B",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  3 * 320,
+  540,
+];
+
+var _B5 = [
+  "5B",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (5 - 1) * 320,
+  540,
+];
+
+var _B6 = [
+  "6B",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (6 - 1) * 320,
+  540,
+];
+
+var _B7 = [
+  "7B",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (7 - 1) * 320,
+  540,
+];
+
+var _B8 = [
+  "8B",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (8 - 1) * 320,
+  540,
+];
+
+var _C1 = [
+  "1C",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  0 * 320,
+  1080,
+];
+
+var _C2 = [
+  "2C",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  1 * 320,
+  1080,
+];
+
+var _C3 = [
+  "3C",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  2 * 320,
+  1080,
+];
+
+var _C4 = [
+  "4C",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  3 * 320,
+  1080,
+];
+
+var _C5 = [
+  "5C",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (5 - 1) * 320,
+  1080,
+];
+
+var _C6 = [
+  "6C",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (6 - 1) * 320,
+  1080,
+];
+
+var _C7 = [
+  "7C",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (7 - 1) * 320,
+  1080,
+];
+
+var _C8 = [
+  "8C",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (8 - 1) * 320,
+  1080,
+];
+
+var _D1 = [
+  "1D",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  0 * 320,
+  1620,
+];
+
+var _D2 = [
+  "2D",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  1 * 320,
+  1620,
+];
+
+var _D3 = [
+  "3D",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  2 * 320,
+  1620,
+];
+
+var _D4 = [
+  "4D",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  3 * 320,
+  1620,
+];
+
+var _D5 = [
+  "5D",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (5 - 1) * 320,
+  1620,
+];
+
+var _D6 = [
+  "6D",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (6 - 1) * 320,
+  1620,
+];
+
+var _D7 = [
+  "7D",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (7 - 1) * 320,
+  1620,
+];
+
+var _D8 = [
+  "8D",
+  "Name_Name_Name_Name",
+  16,
+  333,
+  5555,
+  222,
+  "assets/images/flotteFlitzer/flotteFlitzer.jpg",
+  2,
+  3,
+  (8 - 1) * 320,
+  1620,
+];
