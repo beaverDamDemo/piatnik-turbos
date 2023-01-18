6461a746ae7ce7007c365850ae33a49eb9512b39
zdenevuk <zdenko.vuk@.ijs.si>
2023-01-18 18:26:51 +0100

    added vainas

---
+++ assets/js/src/sloescort.js
@@ -0,0 +1,458 @@
+// var backgroundSize = {
+//   width: 5400,
+//   height: 4500
+// }
+var backgroundSize = {
+  width: 1890,
+  height: 1575,
+};
+
+// 0, 210, 420, 630, 315, 1050, 1260, 1470
+// 8d je 1470, 2520
+var _A1 = [
+  "1A",
+  "Sbarro Challenge III",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  0,
+  0,
+];
+
+var _A2 = [
+  "2A",
+  "Jehle Pantera",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  210,
+  0,
+];
+
+var _A3 = [
+  "3A",
+  "Ford Probe GT",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  420,
+  0,
+];
+
+var _A4 = [
+  "4A",
+  "Porsche 959",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  630,
+  0,
+];
+
+var _A5 = [
+  "5A",
+  "Ferrari Testarossa",
+  1111112,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  315,
+  0,
+];
+
+var _A6 = [
+  "6A",
+  "Jehle Saphir",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1050,
+  0,
+];
+
+var _A7 = [
+  "7A",
+  "Cizeta Moroder V16T",
+
+  1111116,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  1,
+  1260,
+  0,
+];
+
+var _A8 = [
+  "8A",
+  "Midas Gold",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  1,
+  1470,
+  0,
+];
+
+var _B1 = [
+  "1B",
+  "Ferrari F 40",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  0,
+  315,
+];
+
+var _B2 = [
+  "2B",
+  "Callaway Corvette Twin Turbo",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  210,
+  315,
+];
+
+var _B3 = [
+  "3B",
+  "Vector W2 Twin Turbo",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  420,
+  315,
+];
+
+var _B4 = [
+  "4B",
+  "Sbarro Ferrari P4",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  630,
+  315,
+];
+
+var _B5 = [
+  "5B",
+  "Lamborghini Countach",
+  1111112,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  315,
+  315,
+];
+
+var _B6 = [
+  "6B",
+  "Sethera",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1050,
+  315,
+];
+
+var _B7 = [
+  "7B",
+  "Alfa Romeo SZ",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1260,
+  315,
+];
+
+var _B8 = [
+  "8B",
+  "Ford Sierra RS Cosworth",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1470,
+  315,
+];
+
+var _C1 = [
+  "1C",
+  "Arrow C1",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  0,
+  630,
+];
+
+var _C2 = [
+  "2C",
+  "Irmscher GT",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  210,
+  630,
+];
+
+var _C3 = [
+  "3C",
+  "Aston Martin Zagato",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  420,
+  630,
+];
+
+var _C4 = [
+  "4C",
+  "Cadillac Allante",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  630,
+  630,
+];
+
+var _C5 = [
+  "5C",
+  "Isdera Imperator 108i",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  1,
+  3,
+  315,
+  630,
+];
+
+var _C6 = [
+  "6C",
+  "De Tomaso Pantera",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1050,
+  630,
+];
+
+var _C7 = [
+  "7C",
+  "Koenig Mercedes 560 SEC",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1260,
+  630,
+];
+
+var _C8 = [
+  "8C",
+  "Lotus Esprit Turbo",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1470,
+  630,
+];
+
+var _D1 = [
+  "1D",
+  "Alfa Romeo Spider",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  0,
+  945,
+];
+
+var _D2 = [
+  "2D",
+  "Maserati Karif",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  210,
+  945,
+];
+
+var _D3 = [
+  "3D",
+  "Ferrari GTO",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  420,
+  945,
+];
+
+var _D4 = [
+  "4D",
+  "Lamborghini Jalpa",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  630,
+  945,
+];
+
+var _D5 = [
+  "5D",
+  "Pontiac Firebird GTA",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  315,
+  945,
+];
+
+var _D6 = [
+  "6D",
+  "TVR 420 Saloon",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1050,
+  945,
+];
+var _D7 = [
+  "7D",
+  "Chevrolet Camaro",
+  555555,
+  666666,
+  777777,
+  888888,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1260,
+  945,
+];
+
+var _D8 = [
+  "8D",
+  "MVS Venturi",
+  111111,
+  222222,
+  333333,
+  444444,
+  "assets/images/sloescort/sloescort loaded.jpg",
+  2,
+  3,
+  1470,
+  945,
+];
