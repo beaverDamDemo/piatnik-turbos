5aa5a1952c73f0a4eea9cf98bc58e90c15f9c476
zdenevuk <zdenko.vuk@.ijs.si>
2023-01-18 18:44:48 +0100

    wip

--- assets/js/src/sloescort.js
+++ assets/js/src/sloescort.js
@@ -6,375 +6,138 @@ var backgroundSize = {
 
 
 Petra1
-Ljubljana
-70€€/30min 100€/h
 45 let
-Tel.: 070315624
-Zadnja spr.: 03.01.2023
 168cm / 55kg / 3
-6.08 / 7.62 / 6.38
-Zdr.potrdilo: Ne
 
- Petra1
-[Prilepljen profil]   [Dosegljiva]  Maja 6
-Ljubljana
-70€/30min 100€/h
+Maja 6
 56 let
-Tel.: 071531430
-Zadnja spr.: Včeraj
 170cm / 73kg / 5
-0 / 0 / 0
-Zdr.potrdilo: Ne
 
- Maja 6
-[Dosegljiva]  Vesna
-Ljubljana
-Ena ura 130e pol ure 80e
+Vesna
 28 let
-Tel.: 070154706
-Zadnja spr.: Danes
 168cm / 53kg / 3
-8.13 / 6.1 / 5.42
-Zdr.potrdilo: Ne
 
- Vesna
-[Dosegljiva]  Tetyana
-Primorska Nova gorica
-80 euro30 min 130/h Vse ta cene za seks iz zascito
+Tetyana
 48 let
-Tel.: 031661266
-Zadnja spr.: Včeraj
 160cm / 60kg / 4
-7.4 / 8.6 / 8.8
-Zdr.potrdilo: Ne
 
- Tetyana
-[Dosegljiva]  Mia
-Ljubljana Ljubljana
-150€ (90min) - - 100€ ( 50min)
-43 let
-Tel.: 041222105
-Zadnja spr.: Včeraj
-160cm / 57kg / 3
-5.67 / 6.45 / 5.49
-Zdr.potrdilo: Ne
-
- Mia
-Lara 5
-Ljubljana Medno
-TROJCEK 120€/30MIN GFE
-42 let
-Tel.: 051793726
-Zadnja spr.: 16.01.2023
-175cm / 75kg / 2
-8.06 / 9 / 8.33
-Zdr.potrdilo: Ne
-
- Lara 5
-Tea2
-Dolenjska Čateške toplice
-45 min 100€
-43 let
-Tel.: 069970896
-Zadnja spr.: 16.01.2023
-170cm / 65kg / 4
-7.01 / 8.81 / 8.19
-Zdr.potrdilo: Ne
-
- Tea2
-Novo Soraya17
-Ljubljana
-100€/30min 200€/h
-28 let
-Tel.: 071562611
-Zadnja spr.: 15.01.2023
-170cm / 57kg / 2
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Soraya17
-Olga aka Solzica
-Gorenjska
-30€-30min, 50€ -1ura
-68 let
-Tel.: 031637204
-Zadnja spr.: 15.01.2023
-150cm / 58kg / 3
-5.07 / 9.07 / 8.6
-Zdr.potrdilo: Ne
-
- Olga aka Solzica
-[Dosegljiva]  Natalia
-Ljubljana
-100€/30min
-23 let
-Tel.: 031001457
-Zadnja spr.: 15.01.2023
-165cm / 56kg / 4
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
-Vsebuje video predstavitev Natalia
-[Dosegljiva]  Sofia
-Ljubljana
-100€/30min
-20 let
-Tel.: 031001356
-Zadnja spr.: 15.01.2023
-162cm / 55kg / 4
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Sofia
-Kasandra
-Ljubljana Bežigrad
-100€/30min 150€/h
-35 let
-Tel.: 040285298
-Zadnja spr.: 14.01.2023
-170cm / 58kg / 2
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Kasandra
-Ketihana
-Celje Prebold
-26 let
-Tel.: 040342885
-Zadnja spr.: 13.01.2023
-170cm / 60kg / 4
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Ketihana
-Novo Sofia
-Ljubljana
-25 let
-Tel.: 071276630
-Zadnja spr.: 13.01.2023
-175cm / 61kg / 4
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Sofia
-Lucia
-Ljubljana Centro
-100€/30min 150€/h
-22 let
-Tel.: 071276634
-Zadnja spr.: 12.01.2023
-162cm / 60kg / 6
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Lucia
-Camila2
-Ljubljana Centro
-80/30m 1h/150
-41 let
-Tel.: 071344790
-Zadnja spr.: 12.01.2023
-160cm / 63kg / 5
-5 / 6.71 / 5.14
-Zdr.potrdilo: Ne
-
- Camila2
-Kelly
-Ljubljana Centro
-70€/30min 130€/h
-51 let
-Tel.: 071344789
-Zadnja spr.: 12.01.2023
-160cm / 65kg / 5
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Kelly
-Jana (aka Samantax)
-Gorenjska Kranj-Škofja loka..
-70€ 30min 80€ 45min 100€ 1h
-56 let
-Tel.: 030416024
-Zadnja spr.: 10.01.2023
-168cm / 50kg / 2
-6.28 / 7.69 / 6.9
-Zdr.potrdilo: Ne
-
-Vsebuje video predstavitev Jana (aka Samantax)
-Goldenfish
-Ljubljana
-100€/30min 150€/h
-38 let
-Tel.: 069954236
-Zadnja spr.: 29.12.2022
-162cm / 52kg / 3
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Goldenfish
-Lara1
-Ljubljana Ljubljana center
-70€/30min 120€/h20min50€
-44 let
-Tel.: 071400328
-Zadnja spr.: 27.12.2022
-170cm / 63kg / 4
-7.5 / 8.63 / 8.13
-Zdr.potrdilo: Ne
-
- Lara1
-Vanja
-Ljubljana Bezigrad
-100€/30min 150€/h
-26 let
-Tel.: 030736008
-Zadnja spr.: 24.12.2022
-170cm / 60kg / 3
-6.3 / 6.8 / 6
-Zdr.potrdilo: Ne
-
- Vanja
-Nera
-Ljubljana Centar
-60€/30 min 100€/1h
-42 let
-Tel.: 068175037
-Zadnja spr.: 18.12.2022
-163cm / 67kg / 5
-6.7 / 9.13 / 8.57
-Zdr.potrdilo: Ne
-
- Nera
-Larilari
-Celje Celje
-100€/30min 160€/h
-29 let
-Tel.: 069816244
-Zadnja spr.: 14.12.2022
-162cm / 50kg / 2
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Larilari
-Eva 7
-Ljubljana
-80e 30min /150e 1h
-34 let
-Tel.: 070306918
-Zadnja spr.: 14.12.2022
-170cm / 62kg / 3
-8.42 / 9.19 / 8.69
-Zdr.potrdilo: Ne
-
- Eva 7
-Tea
-Ljubljana Centar
-100€/30min 160€/h
-21 let
-Tel.: 071382637
-Zadnja spr.: 13.12.2022
-172cm / 60kg / 3
-9.4 / 8.4 / 6.4
-Zdr.potrdilo: Ne
-
- Tea
 Mia
-Ljubljana
-100€/30min 160€/h
-22 let
-Tel.: 071368268
-Zadnja spr.: 13.12.2022
-170cm / 60kg / 3
-9.38 / 9.75 / 9.63
-Zdr.potrdilo: Ne
-
- Mia
-Špelca
-Ljubljana Šiška
-70€/30min 130€/60min
-41 let
-Tel.: 031400331
-Zadnja spr.: 12.12.2022
-173cm / 59kg / 3
-7.78 / 8.79 / 8.12
-Zdr.potrdilo: Ne
-
- Špelca
-Mimi Blue
-Ljubljana
-80€/100€ 30min160€ 1h
-36 let
-Tel.: 071443190
-Zadnja spr.: 07.12.2022
-170cm / 72kg / 4
-8.38 / 8.42 / 8.84
-Zdr.potrdilo: Ne
-
- Mimi Blue
-Elena
-Ljubljana
-150€/h 100€ pol ure
-22 let
-Tel.: 030755150
-Zadnja spr.: 07.12.2022
-175cm / 65kg / 4
-0 / 0 / 0
-Zdr.potrdilo: Ne
-
- Elena
-Teja
-Ljubljana
-80€/30min-150€/h
-38 let
-Tel.: 070297494
-Zadnja spr.: 07.12.2022
-173cm / 60kg / 3
-8.67 / 9.63 / 9.41
-Zdr.potrdilo: Ne
-
- Teja
-Natasha
-Ljubljana Župančičeva jama.
-70€/30min 130€/1h
-35 let
-Tel.: 070352961
-Zadnja spr.: 07.12.2022
-170cm / 64kg / 2
-6.6 / 8.6 / 7.6
-Zdr.potrdilo: Ne
-
- Natasha
-[Dosegljiva]  Tara aka anita36
-Ljubljana Kranj okolica
-GFE / 70€ / 30min / 120 €h/
-49 let
-Tel.: 040241217
-Zadnja spr.: 04.12.2022
-171cm / 80kg / 4
-4.85 / 6.62 / 6.54
-Zdr.potrdilo: Ne
-
- Tara aka anita36
-[Dosegljiva]  Helena11
-Ljubljana Šiška
-70€/30min 120€/h
-51 let
-Tel.: 070206675
-Zadnja spr.: 03.12.2022
-163cm / 58kg / 3
-7.76 / 9.39 / 9.1
-Zdr.potrdilo: Ne
-
- Helena11
-Lola35
-Ljubljana Sentvid
-60€/30 min 120€/1 ura
 43 let
-Tel.: 070111450
-Zadnja spr.: 02.12.2022
+160cm / 57kg / 3
+
+Lara 5
+42 let
+175cm / 75kg / 2
+
+Tea2
+43 let
+170cm / 65kg / 4
+
+Soraya17
+28 let
+170cm / 57kg / 2
+
+Olga aka Solzica
+68 let
+150cm / 58kg / 3
+
+Natalia
+23 let
+165cm / 56kg / 4
+
+Sofia
+20 let
+162cm / 55kg / 4
+
+Kasandra
+35 let
+170cm / 58kg / 2
+
+26 let
+170cm / 60kg / 4
+
+25 let
+175cm / 61kg / 4
+
+Lucia
+22 let
+162cm / 60kg / 6
+
+Camila2
+41 let
+160cm / 63kg / 5
+
+Kelly
+51 let
+160cm / 65kg / 5
+
+Jana (aka Samantax)
+56 let
+168cm / 50kg / 2
+
+Goldenfish
+38 let
+162cm / 52kg / 3
+
+Lara1
+44 let
+170cm / 63kg / 4
+
+Vanja
+26 let
+170cm / 60kg / 3
+
+Nera
+42 let
+163cm / 67kg / 5
+
+Larilari
+29 let
+162cm / 50kg / 2
+
+Eva 7
+34 let
+170cm / 62kg / 3
+
+Tea
+21 let
+172cm / 60kg / 3
+
+Mia
+22 let
+170cm / 60kg / 3
+
+Špelca
+41 let
+173cm / 59kg / 3
+
+Mimi Blue
+36 let
+170cm / 72kg / 4
+
+Elena
+22 let
+175cm / 65kg / 4
+
+Teja
+38 let
+173cm / 60kg / 3
+
+Natasha
+35 let
+170cm / 64kg / 2
+
+Tara aka anita36
+49 let
+171cm / 80kg / 4
+
+Helena11
+51 let
+163cm / 58kg / 3
+
+Lola35
+43 let
 168cm / 55kg / 3
-7.81 / 8.38 / 7.34
-Zdr.potrdilo: Ne
 
 
 
