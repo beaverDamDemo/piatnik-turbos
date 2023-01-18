3be9cd52c1dc0bd0a7c1d0e3630a1e8f0612a8fe
zdenevuk <zdenko.vuk@.ijs.si>
2023-01-18 18:46:06 +0100

    wip 2

--- assets/js/src/sloescort.js
+++ assets/js/src/sloescort.js
@@ -1,143 +1,209 @@
-var backgroundSize = {
+var bacroundSize = {
   width: 1890,
   height: 1575,
 };
 
-
-
 Petra1
-45 let
-168cm / 55kg / 3
+45
+168
+55
+3
 
 Maja 6
-56 let
-170cm / 73kg / 5
+56
+170
+73
+5
 
 Vesna
-28 let
-168cm / 53kg / 3
+28
+168
+53
+3
 
 Tetyana
-48 let
-160cm / 60kg / 4
+48
+160
+60
+4
 
 Mia
-43 let
-160cm / 57kg / 3
+43
+160
+57
+3
 
 Lara 5
-42 let
-175cm / 75kg / 2
+42
+175
+75
+2
 
 Tea2
-43 let
-170cm / 65kg / 4
+43
+170
+65
+4
 
 Soraya17
-28 let
-170cm / 57kg / 2
+28
+170
+57
+2
 
 Olga aka Solzica
-68 let
-150cm / 58kg / 3
+68
+150
+58
+3
 
 Natalia
-23 let
-165cm / 56kg / 4
+23
+165
+56
+4
 
 Sofia
-20 let
-162cm / 55kg / 4
+20
+162
+55
+4
 
 Kasandra
-35 let
-170cm / 58kg / 2
+35
+170
+58
+2
 
-26 let
-170cm / 60kg / 4
+26
+170
+60
+4
 
-25 let
-175cm / 61kg / 4
+25
+175
+61
+4
 
 Lucia
-22 let
-162cm / 60kg / 6
+22
+162
+60
+6
 
 Camila2
-41 let
-160cm / 63kg / 5
+41
+160
+63
+5
 
 Kelly
-51 let
-160cm / 65kg / 5
+51
+160
+65
+5
 
 Jana (aka Samantax)
-56 let
-168cm / 50kg / 2
+56
+168
+50
+2
 
 Goldenfish
-38 let
-162cm / 52kg / 3
+38
+162
+52
+3
 
 Lara1
-44 let
-170cm / 63kg / 4
+44
+170
+63
+4
 
 Vanja
-26 let
-170cm / 60kg / 3
+26
+170
+60
+3
 
 Nera
-42 let
-163cm / 67kg / 5
+42
+163
+67
+5
 
 Larilari
-29 let
-162cm / 50kg / 2
+29
+162
+50
+2
 
 Eva 7
-34 let
-170cm / 62kg / 3
+34
+170
+62
+3
 
 Tea
-21 let
-172cm / 60kg / 3
+21
+172
+60
+3
 
 Mia
-22 let
-170cm / 60kg / 3
+22
+170
+60
+3
 
 Špelca
-41 let
-173cm / 59kg / 3
+41
+173
+59
+3
 
 Mimi Blue
-36 let
-170cm / 72kg / 4
+36
+170
+72
+4
 
 Elena
-22 let
-175cm / 65kg / 4
+22
+175
+65
+4
 
 Teja
-38 let
-173cm / 60kg / 3
+38
+173
+60
+3
 
 Natasha
-35 let
-170cm / 64kg / 2
+35
+170
+64
+2
 
 Tara aka anita36
-49 let
-171cm / 80kg / 4
+49
+171
+80
+4
 
 Helena11
-51 let
-163cm / 58kg / 3
+51
+163
+58
+3
 
 Lola35
-43 let
-168cm / 55kg / 3
+43
+168
+55
+3
 
 
 
