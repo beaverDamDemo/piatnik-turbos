cc891bd77c4231e9d095a1689d579a9fb4c7e806
zdenevuk <zdenko.vuk@.ijs.si>
2023-01-18 18:40:16 +0100

    working on sloescort

--- assets/js/src/sloescort.js
+++ assets/js/src/sloescort.js
@@ -1,22 +1,391 @@
-// var backgroundSize = {
-//   width: 5400,
-//   height: 4500
-// }
 var backgroundSize = {
   width: 1890,
   height: 1575,
 };
 
-// 0, 210, 420, 630, 315, 1050, 1260, 1470
-// 8d je 1470, 2520
+
+
+Petra1
+Ljubljana
+70€€/30min 100€/h
+45 let
+Tel.: 070315624
+Zadnja spr.: 03.01.2023
+168cm / 55kg / 3
+6.08 / 7.62 / 6.38
+Zdr.potrdilo: Ne
+
+ Petra1
+[Prilepljen profil]   [Dosegljiva]  Maja 6
+Ljubljana
+70€/30min 100€/h
+56 let
+Tel.: 071531430
+Zadnja spr.: Včeraj
+170cm / 73kg / 5
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Maja 6
+[Dosegljiva]  Vesna
+Ljubljana
+Ena ura 130e pol ure 80e
+28 let
+Tel.: 070154706
+Zadnja spr.: Danes
+168cm / 53kg / 3
+8.13 / 6.1 / 5.42
+Zdr.potrdilo: Ne
+
+ Vesna
+[Dosegljiva]  Tetyana
+Primorska Nova gorica
+80 euro30 min 130/h Vse ta cene za seks iz zascito
+48 let
+Tel.: 031661266
+Zadnja spr.: Včeraj
+160cm / 60kg / 4
+7.4 / 8.6 / 8.8
+Zdr.potrdilo: Ne
+
+ Tetyana
+[Dosegljiva]  Mia
+Ljubljana Ljubljana
+150€ (90min) - - 100€ ( 50min)
+43 let
+Tel.: 041222105
+Zadnja spr.: Včeraj
+160cm / 57kg / 3
+5.67 / 6.45 / 5.49
+Zdr.potrdilo: Ne
+
+ Mia
+Lara 5
+Ljubljana Medno
+TROJCEK 120€/30MIN GFE
+42 let
+Tel.: 051793726
+Zadnja spr.: 16.01.2023
+175cm / 75kg / 2
+8.06 / 9 / 8.33
+Zdr.potrdilo: Ne
+
+ Lara 5
+Tea2
+Dolenjska Čateške toplice
+45 min 100€
+43 let
+Tel.: 069970896
+Zadnja spr.: 16.01.2023
+170cm / 65kg / 4
+7.01 / 8.81 / 8.19
+Zdr.potrdilo: Ne
+
+ Tea2
+Novo Soraya17
+Ljubljana
+100€/30min 200€/h
+28 let
+Tel.: 071562611
+Zadnja spr.: 15.01.2023
+170cm / 57kg / 2
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Soraya17
+Olga aka Solzica
+Gorenjska
+30€-30min, 50€ -1ura
+68 let
+Tel.: 031637204
+Zadnja spr.: 15.01.2023
+150cm / 58kg / 3
+5.07 / 9.07 / 8.6
+Zdr.potrdilo: Ne
+
+ Olga aka Solzica
+[Dosegljiva]  Natalia
+Ljubljana
+100€/30min
+23 let
+Tel.: 031001457
+Zadnja spr.: 15.01.2023
+165cm / 56kg / 4
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+Vsebuje video predstavitev Natalia
+[Dosegljiva]  Sofia
+Ljubljana
+100€/30min
+20 let
+Tel.: 031001356
+Zadnja spr.: 15.01.2023
+162cm / 55kg / 4
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Sofia
+Kasandra
+Ljubljana Bežigrad
+100€/30min 150€/h
+35 let
+Tel.: 040285298
+Zadnja spr.: 14.01.2023
+170cm / 58kg / 2
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Kasandra
+Ketihana
+Celje Prebold
+26 let
+Tel.: 040342885
+Zadnja spr.: 13.01.2023
+170cm / 60kg / 4
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Ketihana
+Novo Sofia
+Ljubljana
+25 let
+Tel.: 071276630
+Zadnja spr.: 13.01.2023
+175cm / 61kg / 4
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Sofia
+Lucia
+Ljubljana Centro
+100€/30min 150€/h
+22 let
+Tel.: 071276634
+Zadnja spr.: 12.01.2023
+162cm / 60kg / 6
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Lucia
+Camila2
+Ljubljana Centro
+80/30m 1h/150
+41 let
+Tel.: 071344790
+Zadnja spr.: 12.01.2023
+160cm / 63kg / 5
+5 / 6.71 / 5.14
+Zdr.potrdilo: Ne
+
+ Camila2
+Kelly
+Ljubljana Centro
+70€/30min 130€/h
+51 let
+Tel.: 071344789
+Zadnja spr.: 12.01.2023
+160cm / 65kg / 5
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Kelly
+Jana (aka Samantax)
+Gorenjska Kranj-Škofja loka..
+70€ 30min 80€ 45min 100€ 1h
+56 let
+Tel.: 030416024
+Zadnja spr.: 10.01.2023
+168cm / 50kg / 2
+6.28 / 7.69 / 6.9
+Zdr.potrdilo: Ne
+
+Vsebuje video predstavitev Jana (aka Samantax)
+Goldenfish
+Ljubljana
+100€/30min 150€/h
+38 let
+Tel.: 069954236
+Zadnja spr.: 29.12.2022
+162cm / 52kg / 3
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Goldenfish
+Lara1
+Ljubljana Ljubljana center
+70€/30min 120€/h20min50€
+44 let
+Tel.: 071400328
+Zadnja spr.: 27.12.2022
+170cm / 63kg / 4
+7.5 / 8.63 / 8.13
+Zdr.potrdilo: Ne
+
+ Lara1
+Vanja
+Ljubljana Bezigrad
+100€/30min 150€/h
+26 let
+Tel.: 030736008
+Zadnja spr.: 24.12.2022
+170cm / 60kg / 3
+6.3 / 6.8 / 6
+Zdr.potrdilo: Ne
+
+ Vanja
+Nera
+Ljubljana Centar
+60€/30 min 100€/1h
+42 let
+Tel.: 068175037
+Zadnja spr.: 18.12.2022
+163cm / 67kg / 5
+6.7 / 9.13 / 8.57
+Zdr.potrdilo: Ne
+
+ Nera
+Larilari
+Celje Celje
+100€/30min 160€/h
+29 let
+Tel.: 069816244
+Zadnja spr.: 14.12.2022
+162cm / 50kg / 2
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Larilari
+Eva 7
+Ljubljana
+80e 30min /150e 1h
+34 let
+Tel.: 070306918
+Zadnja spr.: 14.12.2022
+170cm / 62kg / 3
+8.42 / 9.19 / 8.69
+Zdr.potrdilo: Ne
+
+ Eva 7
+Tea
+Ljubljana Centar
+100€/30min 160€/h
+21 let
+Tel.: 071382637
+Zadnja spr.: 13.12.2022
+172cm / 60kg / 3
+9.4 / 8.4 / 6.4
+Zdr.potrdilo: Ne
+
+ Tea
+Mia
+Ljubljana
+100€/30min 160€/h
+22 let
+Tel.: 071368268
+Zadnja spr.: 13.12.2022
+170cm / 60kg / 3
+9.38 / 9.75 / 9.63
+Zdr.potrdilo: Ne
+
+ Mia
+Špelca
+Ljubljana Šiška
+70€/30min 130€/60min
+41 let
+Tel.: 031400331
+Zadnja spr.: 12.12.2022
+173cm / 59kg / 3
+7.78 / 8.79 / 8.12
+Zdr.potrdilo: Ne
+
+ Špelca
+Mimi Blue
+Ljubljana
+80€/100€ 30min160€ 1h
+36 let
+Tel.: 071443190
+Zadnja spr.: 07.12.2022
+170cm / 72kg / 4
+8.38 / 8.42 / 8.84
+Zdr.potrdilo: Ne
+
+ Mimi Blue
+Elena
+Ljubljana
+150€/h 100€ pol ure
+22 let
+Tel.: 030755150
+Zadnja spr.: 07.12.2022
+175cm / 65kg / 4
+0 / 0 / 0
+Zdr.potrdilo: Ne
+
+ Elena
+Teja
+Ljubljana
+80€/30min-150€/h
+38 let
+Tel.: 070297494
+Zadnja spr.: 07.12.2022
+173cm / 60kg / 3
+8.67 / 9.63 / 9.41
+Zdr.potrdilo: Ne
+
+ Teja
+Natasha
+Ljubljana Župančičeva jama.
+70€/30min 130€/1h
+35 let
+Tel.: 070352961
+Zadnja spr.: 07.12.2022
+170cm / 64kg / 2
+6.6 / 8.6 / 7.6
+Zdr.potrdilo: Ne
+
+ Natasha
+[Dosegljiva]  Tara aka anita36
+Ljubljana Kranj okolica
+GFE / 70€ / 30min / 120 €h/
+49 let
+Tel.: 040241217
+Zadnja spr.: 04.12.2022
+171cm / 80kg / 4
+4.85 / 6.62 / 6.54
+Zdr.potrdilo: Ne
+
+ Tara aka anita36
+[Dosegljiva]  Helena11
+Ljubljana Šiška
+70€/30min 120€/h
+51 let
+Tel.: 070206675
+Zadnja spr.: 03.12.2022
+163cm / 58kg / 3
+7.76 / 9.39 / 9.1
+Zdr.potrdilo: Ne
+
+ Helena11
+Lola35
+Ljubljana Sentvid
+60€/30 min 120€/1 ura
+43 let
+Tel.: 070111450
+Zadnja spr.: 02.12.2022
+168cm / 55kg / 3
+7.81 / 8.38 / 7.34
+Zdr.potrdilo: Ne
+
+
+
 var _A1 = [
   "1A",
-  "Sbarro Challenge III",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   0 - 60,
@@ -25,12 +394,12 @@ var _A1 = [
 
 var _A2 = [
   "2A",
-  "Jehle Pantera",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   210 - 60,
@@ -39,12 +408,12 @@ var _A2 = [
 
 var _A3 = [
   "3A",
-  "Ford Probe GT",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   420 - 60,
@@ -53,12 +422,12 @@ var _A3 = [
 
 var _A4 = [
   "4A",
-  "Porsche 959",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   630 - 60,
@@ -67,12 +436,12 @@ var _A4 = [
 
 var _A5 = [
   "5A",
-  "Ferrari Testarossa",
+  "xxxxxxxxxxxxxxxxxx",
   1111112,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   315 - 60,
@@ -81,12 +450,12 @@ var _A5 = [
 
 var _A6 = [
   "6A",
-  "Jehle Saphir",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1050 - 60,
@@ -95,13 +464,13 @@ var _A6 = [
 
 var _A7 = [
   "7A",
-  "Cizeta Moroder V16T",
+  "xxxxxxxxxxxxxxxxxx",
 
   1111116,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   1,
   1260 - 60,
@@ -110,12 +479,12 @@ var _A7 = [
 
 var _A8 = [
   "8A",
-  "Midas Gold",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   1,
   1470 - 60,
@@ -124,12 +493,12 @@ var _A8 = [
 
 var _B1 = [
   "1B",
-  "Ferrari F 40",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   0 - 60,
@@ -138,12 +507,12 @@ var _B1 = [
 
 var _B2 = [
   "2B",
-  "Callaway Corvette Twin Turbo",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   210 - 60,
@@ -152,12 +521,12 @@ var _B2 = [
 
 var _B3 = [
   "3B",
-  "Vector W2 Twin Turbo",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   420 - 60,
@@ -166,12 +535,12 @@ var _B3 = [
 
 var _B4 = [
   "4B",
-  "Sbarro Ferrari P4",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   630 - 60,
@@ -180,12 +549,12 @@ var _B4 = [
 
 var _B5 = [
   "5B",
-  "Lamborghini Countach",
+  "xxxxxxxxxxxxxxxxxx",
   1111112,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   315 - 60,
@@ -194,12 +563,12 @@ var _B5 = [
 
 var _B6 = [
   "6B",
-  "Sethera",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1050 - 60,
@@ -208,12 +577,12 @@ var _B6 = [
 
 var _B7 = [
   "7B",
-  "Alfa Romeo SZ",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1260 - 60,
@@ -222,12 +591,12 @@ var _B7 = [
 
 var _B8 = [
   "8B",
-  "Ford Sierra RS Cosworth",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1470 - 60,
@@ -236,12 +605,12 @@ var _B8 = [
 
 var _C1 = [
   "1C",
-  "Arrow C1",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   0 - 60,
@@ -250,12 +619,12 @@ var _C1 = [
 
 var _C2 = [
   "2C",
-  "Irmscher GT",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   210 - 60,
@@ -264,12 +633,12 @@ var _C2 = [
 
 var _C3 = [
   "3C",
-  "Aston Martin Zagato",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   420 - 60,
@@ -278,12 +647,12 @@ var _C3 = [
 
 var _C4 = [
   "4C",
-  "Cadillac Allante",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   630 - 60,
@@ -292,12 +661,12 @@ var _C4 = [
 
 var _C5 = [
   "5C",
-  "Isdera Imperator 108i",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   1,
   3,
   315 - 60,
@@ -306,12 +675,12 @@ var _C5 = [
 
 var _C6 = [
   "6C",
-  "De Tomaso Pantera",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1050 - 60,
@@ -320,12 +689,12 @@ var _C6 = [
 
 var _C7 = [
   "7C",
-  "Koenig Mercedes 560 SEC",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1260 - 60,
@@ -334,12 +703,12 @@ var _C7 = [
 
 var _C8 = [
   "8C",
-  "Lotus Esprit Turbo",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1470 - 60,
@@ -348,12 +717,12 @@ var _C8 = [
 
 var _D1 = [
   "1D",
-  "Alfa Romeo Spider",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   0 - 60,
@@ -362,12 +731,12 @@ var _D1 = [
 
 var _D2 = [
   "2D",
-  "Maserati Karif",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   210 - 60,
@@ -376,12 +745,12 @@ var _D2 = [
 
 var _D3 = [
   "3D",
-  "Ferrari GTO",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   420 - 60,
@@ -390,12 +759,12 @@ var _D3 = [
 
 var _D4 = [
   "4D",
-  "Lamborghini Jalpa",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   630 - 60,
@@ -404,12 +773,12 @@ var _D4 = [
 
 var _D5 = [
   "5D",
-  "Pontiac Firebird GTA",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   315 - 60,
@@ -418,12 +787,12 @@ var _D5 = [
 
 var _D6 = [
   "6D",
-  "TVR 420 Saloon",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1050 - 60,
@@ -431,12 +800,12 @@ var _D6 = [
 ];
 var _D7 = [
   "7D",
-  "Chevrolet Camaro",
+  "xxxxxxxxxxxxxxxxxx",
   555555,
   666666,
   777777,
   888888,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1260 - 60,
@@ -445,12 +814,12 @@ var _D7 = [
 
 var _D8 = [
   "8D",
-  "MVS Venturi",
+  "xxxxxxxxxxxxxxxxxx",
   111111,
   222222,
   333333,
   444444,
-  "assets/images/vaginas/vaginas loaded.jpg",
+  "assets/images/sloescort/sloescort loaded.png",
   2,
   3,
   1470 - 60,
